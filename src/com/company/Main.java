ccded6e0e89137fb54415dfb02d51e7f1d85daea
cher-di <cher-di@mail.ru>
2020-04-29 23:57:01 +0500

    Grade percentage from pka file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,86 +1,106 @@
 package com.company;
 
 import com.cisco.pt.impl.OptionsManager;
+import com.cisco.pt.ipc.IPCError;
 import com.cisco.pt.ipc.IPCFactory;
+import com.cisco.pt.ipc.enums.FileOpenReturnValue;
+import com.cisco.pt.ipc.system.ActivityFile;
 import com.cisco.pt.ipc.ui.IPC;
 import com.cisco.pt.ptmp.ConnectionNegotiationProperties;
 import com.cisco.pt.ptmp.PacketTracerSession;
 import com.cisco.pt.ptmp.PacketTracerSessionFactory;
 import com.cisco.pt.ptmp.impl.PacketTracerSessionFactoryImpl;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import java.io.File;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
+import static org.apache.commons.codec.digest.Md5Crypt.md5Crypt;
 
-
-class Main
-{
-    private static final int PORT = 39000;
+public class Main {
     private static final String HOST = "localhost";
+    private static final int PORT = 39000;
 
-    public static void main(String args[])
-            throws Exception
+    public Main(PacketTracerSession session) {
+    }
+
+    public static void main(String[] args)
+        throws Exception
     {
-        File file;
-        if (args.length != 1) {
-            System.err.println("You need to specify a pka file to grade");
-            System.exit(1);
-        }
-        file = new File(args[0]);
+        String filepath = args[0];
+        var file = new File(filepath);
         System.out.println(file.getCanonicalPath());
 
-        // Start by getting an instance of the PT Session factory
-        PacketTracerSessionFactory packetTracerSessionFactory = PacketTracerSessionFactoryImpl.getInstance();
+        String password = args[1];
+        System.out.println(password);
 
-        // Get the options used to connect to PT
-        ConnectionNegotiationProperties cnp = OptionsManager.getInstance().getConnectOpts();
+        try {
+            // Start by getting an instance of the PT Session factory
+            PacketTracerSessionFactory packetTracerSessionFactory = PacketTracerSessionFactoryImpl.getInstance();
 
-        // Modify the default options to specify your application parameters
-        cnp.setAuthenticationSecret("cisco");
-        cnp.setAuthenticationApplication("net.netacad.cisco.javafwtest");
+            // Get the options used to connect to PT
+            ConnectionNegotiationProperties cnp = OptionsManager.getInstance().getConnectOpts();
 
-        // Create a PT session
-        PacketTracerSession packetTracerSession = packetTracerSessionFactory.openSession(HOST, PORT, cnp);
+            // Modify the default options to specify your application parameters
+            cnp.setAuthenticationSecret("cisco");
+            cnp.setAuthenticationApplication("com.company.grader");
+            cnp.setAuthentication(ConnectionNegotiationProperties.MD5_AUTH);
 
-        // Get the top level IPC object to communicate with PT
-        IPCFactory ipcFactory = new IPCFactory(packetTracerSession);
-        final IPC ipc = ipcFactory.getIPC();
+            // Create a PT session
+            PacketTracerSession packetTracerSession = packetTracerSessionFactory.openSession(HOST, PORT, cnp);
 
-//        PacketTracerLauncher launcher = PacketTracerLauncher.getInstance();
-//        Process packet_tracer_process = launcher.launch(PORT, true);
+            // Get the top level IPC object to communicate with PT
+            IPCFactory ipcFactory = new IPCFactory(packetTracerSession);
+            final IPC ipc = ipcFactory.getIPC();
 
+            // Get percentage of completed
+            FileOpenReturnValue status = ipc.appWindow().fileOpen(file.getCanonicalPath());
+            System.out.println(status);
+            var activityFile = (ActivityFile) ipc.appWindow().getActiveFile();
 
-//        PacketTracerSessionFactory session_factory = PacketTracerSessionFactoryImpl.getInstance();
-//        PacketTracerSession session = session_factory.openSession(HOST, PORT);
-////        IPCFactory ipc_factory = new IPCFactory(session);
-////        IPC ipc = ipc_factory.getIPC();
-//
-//        System.out.println(session.getSessionStatus().toString());
-//        if (packet_tracer_process != null) {
-//            packet_tracer_process.destroy();
-//        }
+            // Generate password MD5 hash
+            var challengeKey = activityFile.getChallengeKeyAsInts();
+            var hashedPassword = hashPassword(password, challengeKey);
 
+            // Get percentage
+            var confirmed = activityFile.confirmPassword(hashedPassword);
+            if (confirmed) {
+                System.out.println("Confirmed");
+                var percentage = activityFile.getPercentageComplete();
+                System.out.println("Percentage: " + Double.toString(percentage));
+                System.out.println("Name: " + activityFile.getUserProfile().getName());
+                System.out.println("Email: " + activityFile.getUserProfile().getEmail());
+                System.out.println("PercentageScore: " + activityFile.getPercentageCompleteScore());
+            }
+            else {
+                System.out.println("Not confirmed");
+            }
 
-        int a = ipc.network().getLinkCount();
-        System.out.println(a);
+            // Close session with Packet Tracer
+            packetTracerSession.close();
 
-//        ipc.appWindow().fileOpen(file.getCanonicalPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-//        AppWindow window = ipc.appWindow();
-//        ActivityFileEventRegistry registry = session.getEventManager().getActivityFileEvents();
-//        FileOpenReturnValue status = window.fileOpen(file.getCanonicalPath());
-//        System.out.println(status.getIntValue());
-//        ActivityFileImpl activity = ;
-//        pt.
-//        packettracer = new PacketTracer("localhost", 39000, true);
-//        packettracer.launch();
-//        packettracer.connect();
-//        packettracer.fileOpen(file.getCanonicalPath());
-//
-//        ActivityFile activityfile = packettracer.getActivityFile();
-//        Double s = activityfile.getPercentageComplete();
-//
-//        System.out.println((new StringBuilder()).append(s).append("\t").append(args[0]).toString());
-//
-//        packettracer.shutDown();
+    private static String hashPassword(String password, List<Integer> challengeKey) throws NoSuchAlgorithmException {
+        var salt = challengeKey.toArray();
+        byte[] array = new byte[salt.length];
+        for (int i = 0; i < salt.length; i++) {
+            array[i] = (byte) (int) salt[i];
+        }
+
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.update(array);
+        m.update(password.getBytes());
+        byte s[] = m.digest();
+        String result = "";
+        for (int i = 0; i < s.length; i++) {
+            result += Integer.toHexString((0x000000ff & s[i]) | 0xffffff00).substring(6);
+        }
+        return result.toUpperCase();
     }
 }
