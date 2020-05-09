97f212f20a23d595b57113ef2c300b3b071009e0
Dmitry <cher-di@mail.ru>
2020-05-09 19:20:44 +0500

    Throw exception if can not parse parameter

--- src/com/packettracer/grader/args/Args.java
+++ src/com/packettracer/grader/args/Args.java
@@ -1,6 +1,7 @@
 package com.packettracer.grader.args;
 
 import com.packettracer.grader.Constants;
+import com.packettracer.grader.exceptions.IntegerParsingError;
 import com.packettracer.grader.exceptions.ParseError;
 
 public class Args {
@@ -50,22 +51,35 @@ public class Args {
         return delay;
     }
 
-    private int parsePort(String port) {
-        return port != null ? Integer.parseInt(port) : Constants.DEFAULT_PORT;
+    private int parsePort(String port) throws IntegerParsingError {
+        try {
+            return port != null ? Integer.parseInt(port) : Constants.DEFAULT_PORT;
+        } catch (NumberFormatException e) {
+            throw new IntegerParsingError(String.format("Can not parse port: %s", port), e);
+        }
     }
 
     private String parseHost(String host) {
         return host != null ? host : Constants.DEFAULT_HOST;
     }
 
-    private int parseAttempts(String attempts) {
-        return attempts != null ? Integer.parseInt(attempts) : Constants.DEFAULT_CONNECTION_ATTEMPTS_NUMBER;
+    private int parseAttempts(String attempts) throws IntegerParsingError {
+        try {
+            return attempts != null ? Integer.parseInt(attempts) : Constants.DEFAULT_CONNECTION_ATTEMPTS_NUMBER;
+        } catch (NumberFormatException e) {
+            throw new IntegerParsingError(String.format("Can not parse attempts: %s", attempts), e);
+        }
     }
 
-    private int parseDelay(String delay) throws ParseError {
-        int parsed_delay = delay != null ? Integer.parseInt(delay) : Constants.DEFAULT_CONNECTION_ATTEMPTS_DELAY;
+    private int parseDelay(String delay) throws IntegerParsingError {
+        Integer parsed_delay = null;
+        try {
+            parsed_delay = delay != null ? Integer.parseInt(delay) : Constants.DEFAULT_CONNECTION_ATTEMPTS_DELAY;
+        } catch (NumberFormatException e) {
+            throw new IntegerParsingError(String.format("Can not parse delay: %s", delay));
+        }
         if (parsed_delay < Constants.MIN_CONNECTION_ATTEMPTS_DELAY || parsed_delay > Constants.MAX_CONNECTION_ATTEMPTS_DELAY) {
-            throw new ParseError(String.format("Delay between attempts should be %d <= delay <= %d, got %d",
+            throw new IntegerParsingError(String.format("Delay between attempts should be %d <= delay <= %d, got %d",
                     Constants.MIN_CONNECTION_ATTEMPTS_DELAY, Constants.MAX_CONNECTION_ATTEMPTS_DELAY, parsed_delay));
         }
         return parsed_delay;
